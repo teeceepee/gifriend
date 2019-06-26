af88fe04cfd5c837182f7ce9aa9da7115c277976
Yu Haidong <yuhaidongyhd@gmail.com>
2019-06-26 20:21:26 +0800

    Refactor frame code

--- src/frame.rs
+++ src/frame.rs
@@ -14,6 +14,7 @@ use crate::color_table::ColorTable;
 //    Previous = 3,
 //}
 
+/// 解析后的每一帧，`delay_time` 的单位是 10ms，`bytes` 保存的是 RGBA 格式（每像素四字节）的图像数据
 pub struct Frame {
     pub delay_time: u16,
     //pub dispose_method: u8,
@@ -27,40 +28,32 @@ impl Frame {
         screen_height: u16,
         color_table: &ColorTable,
         ctrl: Option<GraphicControlExtension>,
-        img: &TableBasedImage
+        img: &TableBasedImage,
     ) -> Self {
-
-
-        if let Some(control) = ctrl {
-            let transparent_index = if control.transparent_color_flag() == 1 {
+        // graphic control extension 是可选的，
+        // 如果存在，从中取出延迟时间和透明色下标；如果不存在，分别取 0 和 None
+        let (delay_time, transparent_index) = if let Some(control) = ctrl {
+            let tc_index = if control.transparent_color_flag() == 1 {
                 Some(control.transparent_color_index)
             } else {
                 None
             };
 
-            let bytes = img.convert_to_frame(
-                screen_width,
-                screen_height,
-                color_table,
-                transparent_index,
-            );
-
-            Self {
-                delay_time: control.delay_time,
-                bytes,
-            }
+            (control.delay_time, tc_index)
         } else {
-            let mut bytes = img.convert_to_frame(
-                screen_width,
-                screen_height,
-                color_table,
-                None,
-            );
+            (0, None)
+        };
 
-            Self {
-                delay_time: 0,
-                bytes,
-            }
+        let bytes = img.convert_to_frame(
+            screen_width,
+            screen_height,
+            color_table,
+            transparent_index,
+        );
+
+        Self {
+            delay_time,
+            bytes,
         }
     }
 }
