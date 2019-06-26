557b7e93276704a2a7d9881e24996bb14c7c9805
Yu Haidong <yuhaidongyhd@gmail.com>
2019-06-26 20:03:42 +0800

    Add more tests

--- src/lib.rs
+++ src/lib.rs
@@ -22,9 +22,71 @@ mod data_item;
 mod frame;
 mod utils;
 
+// https://www.w3.org/Graphics/GIF/spec-gif89a.txt
+
 pub use frame::Frame;
 pub use gif::Gif;
 
 pub fn parse(gif_bytes: &[u8]) -> std::io::Result<Gif> {
     Gif::parse(gif_bytes)
+}
+
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_parse1() {
+        // 2x2 GIF（原文件的路径 test_samples/2x2.gif）
+        let gif_bytes = [
+            71, 73, 70, 56, 57, 97,        // header
+            2, 0, 2, 0, 128, 0, 0,         // logical screen descriptor
+            255, 255, 255, 0, 0, 0,        // global color table
+            33, 249, 4, 4, 0, 0, 0, 0,     // graph control extension
+
+            44, 0, 0, 0, 0, 2, 0, 2, 0, 0, // image descriptor
+            2,                             // lzw min code size
+            3, 12, 16, 5, 0,               // data blocks [12, 16, 5]
+            59,                            // trailer
+        ];
+
+        if let Ok(gif) = parse(&gif_bytes) {
+            assert_eq!(2, gif.width());
+            assert_eq!(2, gif.height());
+            assert_eq!(1, gif.frames.len());
+
+            let expected_frame: [u8; 16] = [
+                0, 0, 0, 255,
+                255, 255, 255, 255,
+                255, 255, 255, 255,
+                0, 0, 0, 255,
+            ];
+
+            let f = gif.frames.first().unwrap();
+            assert_eq!(expected_frame, f.bytes.as_slice());
+        } else {
+            assert!(false);
+        }
+    }
+
+    #[test]
+    fn test_parse2() -> std::io::Result<()> {
+        let path = "test_samples/beacon.gif";
+
+        let mut file = std::fs::File::open(path)?;
+        let mut bytes = Vec::new();
+        use std::io::Read;
+        file.read_to_end(&mut bytes)?;
+
+        if let Ok(gif) = parse(&bytes) {
+            assert_eq!(6, gif.width());
+            assert_eq!(6, gif.height());
+            assert_eq!(2, gif.frames.len());
+        } else {
+            panic!();
+        }
+
+        Ok(())
+    }
 }
--- src/utils.rs
+++ src/utils.rs
@@ -1,6 +1,6 @@
 use lzw::{Decoder, LsbReader};
 
-// TODO remove unwrap
+/// LZW 解压缩
 pub fn lzw_decode(compressed_data: &[u8], min_code_size: u8) -> Vec<u8> {
     let mut decoded: Vec<u8> = Vec::new();
 
@@ -8,6 +8,7 @@ pub fn lzw_decode(compressed_data: &[u8], min_code_size: u8) -> Vec<u8> {
     let mut compressed = &compressed_data[..];
 
     while compressed.len() > 0 {
+        // TODO to remove unwrap
         let (start, bytes) = decoder.decode_bytes(&compressed).unwrap();
 
         compressed = &compressed[start..];
@@ -15,4 +16,20 @@ pub fn lzw_decode(compressed_data: &[u8], min_code_size: u8) -> Vec<u8> {
     }
 
     decoded
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_decode() {
+        let compressed = [12, 16, 5];
+        let min_code_size = 2;
+
+        let data = lzw_decode(&compressed, min_code_size);
+
+        let expected: [u8; 4] = [1, 0, 0, 1];
+        assert_eq!(expected, data.as_slice());
+    }
 }
